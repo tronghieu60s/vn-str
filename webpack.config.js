a38d5bf896de3c2082fe296f719663aedf0ab0d6
Trọng Hiếu <tronghieu60s@gmail.com>
2021-06-09 09:26:46 +0700

    update demo github pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: './index.js',
+  entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
