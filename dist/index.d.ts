64fd8b2b5e09b88ba387d0d7f464f949d5e600fd
Trong Hieu <tronghieu60s@gmail.com>
2022-01-18 09:12:43 +0700

    fix typescript

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
-export declare function isVietnamesePhoneNumber(str: string): string;
+export declare function isVietnamesePhoneNumber(str: string): boolean;
+export declare function isVietnameseTones(str: string): boolean;
 export declare function numToText(num: number): string;
 export declare function rmNonAlphanumeric(str: string): string;
 export declare function rmVnTones(str: string): string;
